6102dadbd7c7ab16c1154718c00dff12d4981229
nijin39 <nijin39@gmail.com>
2017-07-28 18:16:35 +0900

    Create transmiter.ino

---
+++ UNO/trasnmit.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>
+#include "RF24.h"
+
+bool radioNumber = 0;
+RF24 radio(7,8);
+
+byte addresses[][6] = {"1Node","2Node"};
+
+bool role = 1;
+
+void setup() {
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+  radio.startListening();
+}
+
+void loop() {
+  
+  
+/****************** Ping Out Role ***************************/  
+if (role == 1)  {
+    
+    radio.stopListening();                                    // First, stop listening so we can talk.
+    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
+     if (!radio.write( &start_time, sizeof(unsigned long) )){
+     }
+        
+    radio.startListening();                                    // Now, continue listening
+    
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        //Serial.println(F("Failed, response timed out."));
+    }else{
+        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
+        radio.read( &got_time, sizeof(unsigned long) );
+        unsigned long end_time = micros();
+    }
+    delay(1000);
+  }
+} // Loop
+
